3ec35a3a9127c46f0e0ef393e0b25e94fc5b8b1d
JJ Wendt <jj@theclickmag.net>
2013-07-12 15:16:51 -0700

    Installed devise, added user etc.

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,9 @@ module Treebook
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    #Devise config for Heroku hosting???
+    config.assets.initialize_on_precompile = false
+
   end
 end
